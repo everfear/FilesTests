5b74307d256960988d537d85680be13c4d2e8ca0
Sergei <sergeyevrfr@gmail.com>
2024-05-26 16:23:15 +0400

    update project

--- src/test/java/FilesTestsPack/FilesTest.java
+++ src/test/java/FilesTestsPack/FilesTest.java
@@ -1,4 +1,23 @@
 package FilesTestsPack;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class FilesTest {
+
+    @Test
+    void downloadFileTest() {
+
+        open("https://github.com/junit-team/junit5/blob/main/README.md");
+        File downloaded =
+                $(".react-blob-header-edit-and-raw-actions [href*='main/README.md']")
+                        .download();
+
+        System.out.println();
+
+    }
 }
